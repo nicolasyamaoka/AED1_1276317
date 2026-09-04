db929be0426a5667a667567a0edc4bd703db5066
nicolasyamaoka <nicolas.yamaoka@unifesp.br>
2026-09-04 00:34:43 -0300

    Refactor Sudoku validation logic and comments

--- 1383.c
+++ 1383.c
@@ -2,7 +2,7 @@
 Disciplina  : Algortimo e Estrutura de Dados 2026S1
 Nome        : Nicolas Fenalti Yamaoka
 Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Problema    : https://judge.beecrowd.com/pt/problems/view/1383
 Data        : 21/08/2026
 Objetivo    : Verificar se a matriz corresponde as especificações do Sudoku
 Dificuldade : Verificar repetições de termos nas matrizes 3x3.
